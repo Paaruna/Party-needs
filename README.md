add49e63339078374f7b6b3c0815baabd04b4732
Hogwarter <islalodon@gmail.com>
2021-10-05 17:16:20 +0300

    Like you wanted, I hope.
    Added some to the readme-file, an idea, like you asked.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Party-needs
-Helps you count how much you will need for a party.
+Helps you count how much food and alcohol you will need for a party.
+Possible contributions:
+- Eddit so that the user can change what products to include.
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ students = input("How many students are coming to the party?\n")
 students = int(students)
 no_sausage = input("How many of the students don't eat sausages?\n")
 no_sausage = int(no_sausage)
-no_beer = input("How many of the students don't drink beer?\n")
-no_beer = int(no_beer)
+shots = input("How many of the students want shots instead of beer?\n")
+shots = int(shots)
+no_alcohol = input("How many of the students don't drink alcohol?\n")
+no_alcohol = int(no_alcohol)
 
 sausage = (students - no_sausage) * 3
 corncobs = no_sausage * 3
-beer = (students - no_beer) * 4
-cola = no_beer * 4
+beer = (students - no_alcohol - shots) * 4
+shots = int(shots) * 4
+cola = no_alcohol * 4
 
-print("For the party, you will need" , sausage , "sausages,\n" , corncobs , "corncobs,\n" , beer ," beers and\n" , cola , "Cola-bottles.")
+print("For the party, you will need" , sausage , "sausages,\n" , corncobs , "corncobs,\n" , shots, "shots,\n", beer ," beers and\n" , cola , "Cola-bottles.")
